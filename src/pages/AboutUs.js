9c8397a765e4a1907f8b24bbd3ed9e9bb61dc77c
Andrew Tattersall <tattwo@live.com>
2021-03-01 20:17:10 +0000

    2nd commit

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+// Page component
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;
