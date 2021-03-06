8bf7022a1dde121ea200275a930b4a519db45f2f
Andrew Tattersall <tattwo@live.com>
2021-03-06 17:20:14 +0000

    added styling to about us

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
 body{
   background: #1b1b1b;
+  font-family: 'Inter', sans-serif;
 }
 
 button{
@@ -21,6 +22,7 @@ button{
   background: transparent;
   color: white;
   transition: all 0.5s ease;
+  font-family: 'Inter', sans-serif;
   &:hover{
     background: #23d997;
     color: white;
@@ -35,6 +37,7 @@ h2{
   }
   h4{
     font-weight: bold;
+    font-size: 2rem;
   }
   span{
     color: #23d997;
