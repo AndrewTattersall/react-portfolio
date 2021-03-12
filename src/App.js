b519d94b0fe4e7e26b2308fa0c6901a9a3ac7877
Andrew Tattersall <tattwo@live.com>
2021-03-12 19:59:43 +0000

    added movie sections and start animaitons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 //import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
 //import Global style
 import GlobalStyle from "./components/GlobalStyle";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <Link id="logo" to="/">
+          Andrew Tattersall
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">About Me</Link>
+        </li>
+        <li>
+          <Link to="/work">My Work</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background: #282828;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  #logo {
+    font-size: 1.5rem;
+    font-family: "lobster", cursive;
+    font-weight: lighter;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+`;
+
+export default Nav;
